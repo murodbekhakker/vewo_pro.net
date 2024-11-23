6ecfc972d5f86ffd867630e1fd18244ce0bfaae1
murodbek_08 <murodbekarslonov201@gmail.com>
2024-11-23 20:09:04 +0500

    new bac to top

--- js/main.js
+++ js/main.js
@@ -7,3 +7,37 @@ document.addEventListener("DOMContentLoaded", () => {
     navLinks.classList.toggle("active");
   });
 });
+// loader
+const loading = document.getElementById("loading");
+
+const loadingDuration = 3000; // 1.8s
+
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, loadingDuration);
+
+//back to top
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+const myBtn = document.getElementById("myBtn");
+myBtn.onclick = function () {
+  const scrollStep = -window.scrollY / (100 / 180);
+  const scrollInterval = setInterval(() => {
+    if (window.scrollY === 0) {
+      clearInterval(scrollInterval);
+    } else {
+      window.scrollBy(0, scrollStep);
+    }
+  }, 15);
+};
