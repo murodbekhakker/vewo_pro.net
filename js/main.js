f0bbf176340231000a73ef91ae7d9129e36d036e
murodbek_08 <murodbekarslonov201@gmail.com>
2024-11-23 21:28:07 +0500

    new dark mode

--- js/main.js
+++ js/main.js
@@ -41,3 +41,9 @@ myBtn.onclick = function () {
     }
   }, 15);
 };
+// dark-mode
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
